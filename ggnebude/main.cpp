7642f3f8bf95665aafae1cc4d3de5b44d2182482
ZaebalRebutatsa <ZaebalRebutatsa@WIN-1J5P8UF7VA6>
2024-03-08 19:25:31 +0300

    Добавьте файлы проекта.

---
+++ ggnebude/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+void main()
+{
+	setlocale(LC_ALL, "Russian");
+	cout << "Влад Сценарист?\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n";
+	system("pause");
+}
